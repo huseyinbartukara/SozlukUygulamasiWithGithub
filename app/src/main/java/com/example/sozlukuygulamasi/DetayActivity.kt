f8c6d05639bdb45358804a81aa324f1d0ebf3f88
Huseyin Bartu <huseyinbartukara@gmail.com>
2022-11-15 17:22:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sozlukuygulamasi/DetayActivity.kt
@@ -0,0 +1,19 @@
+package com.example.sozlukuygulamasi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detay.*
+
+class DetayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detay)
+
+        val kelime = intent.getSerializableExtra("nesne") as Kelimeler
+
+        textViewDetayIngilizce.text = kelime.ingilizce
+        textViewDetayTurkce.text = kelime.turkce
+
+
+    }
+}
---
+++ app/src/main/java/com/example/sozlukuygulamasi/Kelimeler.kt
@@ -0,0 +1,7 @@
+package com.example.sozlukuygulamasi
+
+import java.io.Serializable
+
+data class Kelimeler (var kelime_id:Int, var ingilizce:String, var turkce:String) : Serializable {
+
+}
